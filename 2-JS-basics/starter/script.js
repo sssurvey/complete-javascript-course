1dfcb70009891603ba3cca8d97c8dec40666391a
haominshi <haominshi@gmail.com>
2020-01-05 18:56:07 -0600

    code challenge 2

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -23,3 +23,30 @@ var john = new Person(172, 60);
 
 console.log("Is Mark's BMI higher than John's BMI?");
 compareMarksBMIToJohn(mark, john);
+
+// coding challenge 2
+var firstTeam = [100, 200, 140];
+var secondTeam = [100, 200, 143];
+
+function calculateWinner(team1, team2) {
+    var avgTeam1 = 0;
+    var avgTeam2 = 0;
+    team1.forEach(element => {
+        avgTeam1 += element;
+    });
+    team2.forEach(element => {
+        avgTeam1 += element;
+    });
+    switch (true) {
+        case avgTeam1 < avgTeam2:
+            console.log("Team 2 won");
+            break;
+        case avgTeam1 > avgTeam2:
+            console.log("Team 1 won");
+            break;
+        default:
+            console.log("Draw");
+    }
+}
+
+calculateWinner(firstTeam, secondTeam);

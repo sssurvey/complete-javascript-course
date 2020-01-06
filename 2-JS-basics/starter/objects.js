5bc5511f1aeb85df3bb2c22a9cd9031cd0d18ca5
haominshi <haominshi@gmail.com>
2020-01-05 21:51:26 -0600

    added JS standard style format

--- 2-JS-basics/starter/objects.js
+++ 2-JS-basics/starter/objects.js
@@ -1,26 +1,23 @@
 var john = {
-    firstName: 'John',
-    lastName: 'Smith',
-    birthYear: 1990,
-    family: ['Jane', 'Mark', 'Bob', 'Emily'],
-    job: 'teacher',
-    isMarried: false,
-    calcAge: function(birthYear) {
-        return 2020 - birthYear;
-    },
-    calcAgeDemoThis: function() {
-        return 2020 - this.birthYear;
-    }
-};
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1990,
+  family: ['Jane', 'Mark', 'Bob', 'Emily'],
+  job: 'teacher',
+  isMarried: false,
+  calcAge: function (birthYear) {
+    return 2020 - birthYear
+  },
+  calcAgeDemoThis: function () {
+    return 2020 - this.birthYear
+  }
+}
 
-console.log(john.calcAge(john.birthYear)); // output -> 30
-console.log(john.calcAgeDemoThis()); // same as previous call -> 30
+console.log(john.calcAge(john.birthYear)) // output -> 30
+console.log(john.calcAgeDemoThis()) // same as previous call -> 30
 
 // we can also save the value back to john obj.
 // we can choose to have void method and do it in the method lazily
 // or we can do it like this:
-john.age = john.calcAgeDemoThis();
-console.log(john.age); // as expected, we have output -> 30
-
-
-
+john.age = john.calcAgeDemoThis()
+console.log(john.age) // as expected, we have output -> 30
--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,91 +1,89 @@
 class Person {
+  constructor (height, weight) {
+    this.height = height
+    this.weight = weight
+  }
 
-    constructor(height, weight) {
-        this.height = height;
-        this.weight = weight;
-    }
-
-    calculateBMI() {
-        this.bmi = this.weight / (this.height * this.height);
-    }
-
+  calculateBMI () {
+    this.bmi = this.weight / (this.height * this.height)
+  }
 }
 
-function compareMarksBMIToJohn(p1, p2) {
-    if (p1.bmi > p2.bmi) {
-        console.log("true");
-    } else if (p1.bmi == p2.bmi) {
-        console.log("they are the same");
-    } else {
-        console.log("false");
-    }
+function compareMarksBMIToJohn (p1, p2) {
+  if (p1.bmi > p2.bmi) {
+    console.log('true')
+  } else if (p1.bmi == p2.bmi) {
+    console.log('they are the same')
+  } else {
+    console.log('false')
+  }
 }
 
-var mark = new Person(172, 66);
-var john = new Person(172, 60);
+var mark = new Person(172, 66)
+var john = new Person(172, 60)
 
-mark.calculateBMI();
-john.calculateBMI();
+mark.calculateBMI()
+john.calculateBMI()
 
-console.log("Is Mark's BMI higher than John's BMI?");
-compareMarksBMIToJohn(mark, john);
+console.log("Is Mark's BMI higher than John's BMI?")
+compareMarksBMIToJohn(mark, john)
 
 // coding challenge 2
-var firstTeam = [100, 200, 140];
-var secondTeam = [100, 200, 143];
+var firstTeam = [100, 200, 140]
+var secondTeam = [100, 200, 143]
 
-function calculateWinner(team1, team2) {
-    var avgTeam1 = 0;
-    var avgTeam2 = 0;
-    team1.forEach(element => {
-        avgTeam1 += element;
-    });
-    team2.forEach(element => {
-        avgTeam1 += element;
-    });
-    switch (true) {
-        case avgTeam1 < avgTeam2:
-            console.log("Team 2 won");
-            break;
-        case avgTeam1 > avgTeam2:
-            console.log("Team 1 won");
-            break;
-        default:
-            console.log("Draw");
-    }
+function calculateWinner (team1, team2) {
+  var avgTeam1 = 0
+  var avgTeam2 = 0
+  team1.forEach(element => {
+    avgTeam1 += element
+  })
+  team2.forEach(element => {
+    avgTeam1 += element
+  })
+  switch (true) {
+    case avgTeam1 < avgTeam2:
+      console.log('Team 2 won')
+      break
+    case avgTeam1 > avgTeam2:
+      console.log('Team 1 won')
+      break
+    default:
+      console.log('Draw')
+  }
 }
 
-calculateWinner(firstTeam, secondTeam);
+calculateWinner(firstTeam, secondTeam)
 
 // coding challenge 3
-var amountSpendArr = [23, 55, 108];
+var amountSpendArr = [23, 55, 108]
 
-function calculateTip(intArr) {
-    var tipArr = [];
-    intArr.forEach((e) => {
-            switch(e) {
-                case 0 < e && e < 20:
-                    tipArr.push(e * 0.2);
-                    break;
-                case 20 <= e && e <= 50:
-                    tipArr.push(e * 0.15);
-                    break;
-                default:
-                    tipArr.push(e * 0.1);
-            }
-        });
-    tipArr.forEach((e) => {
-        console.log("You need to pay just tip = " + e + " dollars.");
-    });
+function calculateTip (intArr) {
+  var tipArr = []
+  intArr.forEach((e) => {
+    switch (e) {
+      case e > 0 && e < 20:
+        tipArr.push(e * 0.2)
+        break
+      case e >= 20 && e <= 50:
+        tipArr.push(e * 0.15)
+        break
+      default:
+        tipArr.push(e * 0.1)
+    }
+  })
+  tipArr.forEach((e) => {
+    console.log('You need to pay just tip = ' + e + ' dollars.')
+  })
 
-    var totalAmountArr = [];
-    var counter = 0;
-    intArr.forEach((e)=> {
-        totalAmountArr.push(e + tipArr[counter++]);
-    });    
-    totalAmountArr.forEach((e)=>{
-        console.log("You need to pay total = " + e + " dollars.");
-    });
+  var totalAmountArr = []
+  var counter = 0
+  intArr.forEach((e) => {
+    totalAmountArr.push(e + tipArr[counter++])
+  })
+  totalAmountArr.forEach((e) => {
+    console.log('You need to pay total = ' + e + ' dollars.')
+  })
 }
 
-calculateTip(amountSpendArr);
+calculateTip(amountSpendArr)

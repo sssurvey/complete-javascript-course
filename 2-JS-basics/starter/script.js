6c5e2f5761cf08f92f718469961036abb3f430d5
haominshi <haominshi@gmail.com>
2020-01-05 18:34:50 -0600

    coding challenge 1

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+class Person {
+
+    constructor(height, weight) {
+        this.height = height;
+        this.weight = weight;
+        this.bmi = this.weight / (this.height * this.height);
+    }
+
+}
+
+function compareMarksBMIToJohn(p1, p2) {
+    if (p1.bmi > p2.bmi) {
+        console.log("true");
+    } else if (p1.bmi == p2.bmi) {
+        console.log("they are the same");
+    } else {
+        console.log("false");
+    }
+}
+
+var mark = new Person(172, 66);
+var john = new Person(172, 60);
+
+console.log("Is Mark's BMI higher than John's BMI?");
+compareMarksBMIToJohn(mark, john);

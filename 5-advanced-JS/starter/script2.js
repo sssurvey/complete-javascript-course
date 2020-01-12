20e17f4ff4a1a61653574fa4bd0898a8ae5bcd2f
haominshi <haominshi@gmail.com>
2020-01-12 14:03:27 -0600

    completes class 68

--- 5-advanced-JS/starter/script2.js
+++ 5-advanced-JS/starter/script2.js
@@ -33,16 +33,51 @@ function interviewQuestion(job) {
     }
 }
 
-var teacherQuestion = interviewQuestion('teacher');
-var designerQuestion = interviewQuestion('designer');
+// closures
+function retirement(retirementAge) {
+    var outputString = " years until retirement."
+    return (yearOfBirth) => {
+        var age = 2020 - yearOfBirth;
+        console.log((retirementAge - age) + outputString)
+    }
+}
+
+// re-write interview question with closure:
+function interviewQuestionClosure(job) {
+    return function (name) {
+        switch (job) {
+            case 'designer':
+                console.log(name + ", can you please explain what's UX?");
+                break;
+            case 'teacher':
+                console.log("What subject do you teach, " + name + "?");
+                break;
+            default:
+                console.log("Hello " + name + ", what do you do?");
+                break;
+        }
+    }
+}
 
 // console
 (function consoleOutput() {
+
+    // function that takes a function as param, output below
     console.log(arrayCalculator(years, calculateAge));
     
     // the first fucntion returns a function for us to call it
+    // function that returns a function output below
     interviewQuestion('hacker')('Neo'); 
-
+    var teacherQuestion = interviewQuestion('teacher');
+    var designerQuestion = interviewQuestion('designer');
     teacherQuestion('John');
     designerQuestion('John');
+    console.log('\n')
+
+    // closure outputs below
+    retirement(65)(1996);
+    var teacherQuestion2 = interviewQuestionClosure('teacher');
+    var designerQuestion2 = interviewQuestionClosure('designer');
+    teacherQuestion2('Jane');
+    designerQuestion2('Dan');
 })();

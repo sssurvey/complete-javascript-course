cb7c9f1dddd63386a9362a667e5ee714124742a2
Haomin Shi <haomins1@verifone.com>
2020-02-06 11:39:14 -0600

    completes class 116

--- 7-ES6/starter/classes.js
+++ 7-ES6/starter/classes.js
@@ -1,5 +1,7 @@
+// class in ES6
+
 class People {
-    
+
     constructor(name, age) {
         this.name = name;
         this.age = age;
@@ -19,3 +21,31 @@ class People {
 let people = new People('Ggoogle', 32);
 console.log(people.toString());
 console.log(People.greeting());
+
+// inheritance in ES6
+
+class Athlete extends People {
+
+    constructor(name, age, olympicGames, medals) {
+        super(name, age); // <-------- important call
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+
+    // override to string
+    toString() {
+        return `The athelete's name is: ${this.name}, the age is: ${this.age}.`
+        + ` He has attended ${this.olympicGames} olympicgames and won ${this.medals} medals!`;
+    }
+
+    wonMedal() {
+        this.medals++;
+        console.log(`Now ${this.name} has ${this.medals} medals!`);
+    }
+}
+
+const athlete = new Athlete('John', 23, 3, 10);
+console.log(Athlete.greeting());
+console.log(athlete.toString());
+athlete.wonMedal()
+// console.log();

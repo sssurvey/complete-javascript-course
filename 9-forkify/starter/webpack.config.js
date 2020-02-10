be66f99c633680c2ab27e6a8e891eb50c20468c5
haominshi <haominshi@gmail.com>
2020-02-09 18:13:00 -0600

    changed webpack config

--- 9-forkify/starter/webpack.config.js
+++ 9-forkify/starter/webpack.config.js
@@ -7,8 +7,8 @@ module.exports = {
         path: path.resolve(__dirname, 'dist/js'),
         filename: 'bundle.js'
     },
-    mode: 'development' // <-- this allows us to not optimize too much when bundling
-                        // i.e: not minify the code etc. Helps with the speed
-                        // we can override this via the npm script with webpack
-                        // and not do it here
+    // mode: 'development' // <-- this allows us to not optimize too much when bundling
+    //                     // i.e: not minify the code etc. Helps with the speed
+    //                     // we can override this via the npm script with webpack
+    //                     // and not do it here
 };

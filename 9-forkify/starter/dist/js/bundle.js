b949dc26683283212700bbd3f92ad8e28170b3a4
Haomin Shi <haomins1@verifone.com>
2020-02-10 11:43:16 -0600

    completed class 135 <-- review this

--- 9-forkify/starter/dist/js/bundle.js
+++ 9-forkify/starter/dist/js/bundle.js
@@ -90,23 +90,10 @@
 /*!*************************!*\
   !*** ./src/js/index.js ***!
   \*************************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/*! no static exports found */
+/***/ (function(module, exports) {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _test__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./test */ \"./src/js/test.js\");\n// Global app controller\n\n\nconsole.log(\"xxxxL: \");\nconsole.log(\"xxxxL: \" + _test__WEBPACK_IMPORTED_MODULE_0__[\"default\"]);\n\n//# sourceURL=webpack:///./src/js/index.js?");
-
-/***/ }),
-
-/***/ "./src/js/test.js":
-/*!************************!*\
-  !*** ./src/js/test.js ***!
-  \************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (23);\n\n//# sourceURL=webpack:///./src/js/test.js?");
+eval("// Global app controller\n\n//# sourceURL=webpack:///./src/js/index.js?");
 
 /***/ })
 
--- 9-forkify/starter/webpack.config.js
+++ 9-forkify/starter/webpack.config.js
@@ -1,14 +1,30 @@
 // Object for specfify settings
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: './src/js/index.js', // <-- entry for building the bunlde on the dev side
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist/'), // <-- path to put the bundle, and res
+        filename: 'js/bundle.js' // <-- the actual bundle.js
     },
     // mode: 'development' // <-- this allows us to not optimize too much when bundling
     //                     // i.e: not minify the code etc. Helps with the speed
     //                     // we can override this via the npm script with webpack
     //                     // and not do it here
+    devServer: {
+        contentBase: './dist', // <--- content to serve is set to the ./dist. Since
+                               // since src is only for development, while the dist
+                               // is for deployement.
+                               // <-- this is for the dependency webpack-dev-server
+                               // we don't really need live-server if we have this
+                               // just keep in mind webpack, cli, server are seperate 
+                               // dependencies
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+    ]
 };
